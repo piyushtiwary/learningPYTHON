157e3f831adb9bc7f26b420173c92a68b2f1f218
Pratyush <pratyushtiwary2404@gmail.com>
2022-10-01 16:16:59 +0530

    harami

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -20,30 +20,26 @@ print(" \n Enter 1 for Sum \n Enter 2 for Subtraction \n Enter 3 for Division \n
 userInput = int(input())
 
 if(userInput == 1):
-	print("Enter 2 numbers")
-	var1 = int(input())
-	var2 = int(input())
+	var1 = int(input("Enter 1st number"))
+	var2 = int(input("Enter 2nd number"))
 	Output = myFunSum(var1,var2)
 	print(Output)
 
 elif(userInput == 2):
-	print("Enter 2 numbers")
-	var1 = int(input())
-	var2 = int(input())
+	var1 = int(input("Enter 1st value"))
+	var2 = int(input("Enter 2nd value"))
 	Output = myFunSub(var1,var2)
 	print(Output)
 
 elif(userInput == 3):
-	print("Enter 2 numbers in A/B order")
-	var1 = int(input())
-	var2 = int(input())
+	var1 = int(input("Enter 1st value"))
+	var2 = int(input("Enter 2nd value"))
 	Output = myFunDiv(var1,var2)
 	print(Output)
 
 elif(userInput == 4):
-	print("Enter 2 numbers")
-	var1 = int(input())
-	var2 = int(input())
+	var1 = int(input("Enter 1st value"))
+	var2 = int(input("Enter 2nd value"))
 	Output = myFunMulty(var1,var2)
 	print(Output)
 
