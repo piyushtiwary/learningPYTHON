857a910fa0fde2f8f3289c639cdfeebd4368ed11
Pratyush <pratyushtiwary2404@gmail.com>
2022-10-01 16:17:16 +0530

    hello

--- Lecture.py
+++ Lecture.py
@@ -1,4 +1,6 @@
-x = 5
-y = 10
-sum = x+y
-print(sum)
+x = int(input("Enter the value of x number"))
+y = int(input("Enter the value of y number"))
+z = x
+x= y
+y=z
+print("Swaped value of x is ",x , "swapped value of y is ", y )
