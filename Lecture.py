042a737b949548ef588d695860621633d9c6d9f4
Pratyush <pratyushtiwary2404@gmail.com>
2022-10-02 13:28:06 +0530

    Done

--- Lecture.py
+++ Lecture.py
@@ -1,6 +1,9 @@
-x = int(input("Enter the value of x number"))
-y = int(input("Enter the value of y number"))
-z = x
-x= y
-y=z
-print("Swaped value of x is ",x , "swapped value of y is ", y )
+import math
+x = int(input("Enter the 1st side of of the triangle"))
+y = int(input("Enter the 2nd side of of the triangle"))
+z = int(input("Enter the 3rd side of of the triangle"))
+
+s = (x+y+x)/2
+ini = int(s(s-x)*(s-y)*(s-z))
+area = int(math.sqrt(ini))
+print(area)
---
+++ swapingValues.py
@@ -0,0 +1,12 @@
+# taking input form users
+x = input("Enter the Value of X: ")
+y = input("Enter the Value of Y: ")
+
+# swaping values by using a temp. variable
+temp = x
+x=y
+y=temp
+
+# printing new values
+print("Value of new X is: ",x)
+print("Value of new Y is: ",y)
