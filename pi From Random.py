d18ac049e980b51bc29a6b7b6fb171c43c7a2b8c
Pratyush <pratyushtiwary2404@gmail.com>
2022-10-03 23:22:52 +0530

    lets see

--- pi From Random.py
+++ pi From Random.py
@@ -5,6 +5,7 @@ points = int(input("Enter the number of points "))
 dots_in_circle = 0
 dots_in_square = 0
 
+# counting number of random dots 
 for _ in range(points):
 	x = random.uniform(0,1)
 	y = random.uniform(0,1)
@@ -13,6 +14,7 @@ for _ in range(points):
 		dots_in_circle +=1 
 	dots_in_square +=1
 
+# calculating pi
 pi = 4* dots_in_circle/dots_in_square
 
 print(pi)
