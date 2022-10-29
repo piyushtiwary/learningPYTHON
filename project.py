215b5a5e5f1ce7260276db99e4bad010b9e263ae
Pratyush <pratyushtiwary2404@gmail.com>
2022-10-29 13:59:09 +0530

    star

---
+++ project.py
@@ -0,0 +1,8 @@
+#You need to write a python script that generates an acronym word from a given sentence.
+#•	Take multiple strings as input in the form of list.
+#•	Add the first letter of each string to output.
+#•	Iterate over the complete string and add every next letter to space to output.
+#•	Change the output to uppercase (required acronym).
+#•	You have to generate acronyms for all given strings.
+
+
